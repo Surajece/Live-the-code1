04d9eee9d4fc0983fa50d98adf659003940d8eb6
Devanshe sharma <devanshesharma6@gmail.com>
2023-09-09 21:49:09 +0530

    description page

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+var descriptions = document.querySelectorAll(".description");
+var services = document.querySelectorAll(".services");
+var healthSupport = document.querySelectorAll(".healthSupport");
+var aboutUs = document.querySelectorAll(".aboutUs");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+var currentLink = descriptions[0];
+
+function changeLink(link) {
+  currentLink.style.backgroundColor = "white";
+  link.style.backgroundColor = "skyblue";
+  currentLink = link;
+}
+
+descriptions.forEach(function(description) {
+  description.addEventListener("click", function() {
+    changeLink(this);
+  });
+});
+
+services.forEach(function(service) {
+  service.addEventListener("click", function() {
+    changeLink(this);
+  });
+});
+
+healthSupport.forEach(function(healthSupport) {
+  healthSupport.addEventListener("click", function() {
+    changeLink(this);
+  });
+});
+
+aboutUs.forEach(function(aboutUs) {
+  aboutUs.addEventListener("click", function() {
+    changeLink(this);
+  });
+});
